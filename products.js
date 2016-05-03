549d44f20b23827d186924bfea15c4ef11904623
Kyuhun Lee <thinkong@users.noreply.github.com>
2016-05-03 15:17:11 +0900

    Update products.js

--- products.js
+++ products.js
@@ -3,22 +3,22 @@
     {
       "name": "옥시싹싹",
       "company": "옥시",
-      "reasons": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
       "name": "옥시제품2",
       "company": "옥시",
-      "reasons": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
       "name": "옥시제품1",
       "company": "옥시",
-      "reasons": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
       "name": "뱅뱅",
       "company": "옥시",
-      "reasons": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     }
   ]
 }
