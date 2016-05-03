23afef1e2acce933151c69f2ab0cdfbdb68befdb
Kyuhun Lee <thinkong@users.noreply.github.com>
2016-05-03 14:27:46 +0900

    test3

--- products.js
+++ products.js
@@ -8,7 +8,14 @@
       ]
     },
     {
-      "name": "옥시제품.....",
+      "name": "옥시제품2",
+      "company": "옥시",
+      "reasons": [
+        "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      ]
+    },
+    {
+      "name": "옥시제품1",
       "company": "옥시",
       "reasons": [
         "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
