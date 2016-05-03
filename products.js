fd7f616f73a56cb4f2d918c14352e3849ca0e48d
Kyuhun Lee <thinkong@users.noreply.github.com>
2016-05-03 16:52:22 +0900

    Update products.js

--- products.js
+++ products.js
@@ -1,23 +1,23 @@
 {
   "data": [
     {
-      "name": "옥시싹싹",
-      "company": "옥시",
+      "name": "OxySakSak",
+      "company": "Oxy",
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
-      "name": "옥시제품2",
-      "company": "옥시",
+      "name": "Oxy2",
+      "company": "Oxy",
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
-      "name": "옥시제품1",
-      "company": "옥시",
+      "name": "Oxy3",
+      "company": "Oxy",
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
-      "name": "뱅뱅",
-      "company": "옥시",
+      "name": "Oxy4",
+      "company": "Oxy",
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     }
   ]
