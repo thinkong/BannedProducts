7034bd5da65f78cc6e501c9c09c10a2566a16dec
Kyuhun Lee <thinkong@users.noreply.github.com>
2016-05-04 17:51:57 +0900

    Update products.js

--- products.js
+++ products.js
@@ -1,64 +1,81 @@
 {
+  "incident":[
+    {
+      "id":1,
+    "company":"옥시레킷벤키저",
+    "reason":"http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+    }
+    ],
   "data": [
     {
       "name": "파워크린",
       "company": "옥시레킷벤키저",
-      "replacement": "비트, 수퍼타이 등"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "비트, 수퍼타이 등",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "옥시크린",
       "company": "옥시레킷벤키저",
-      "replacement": "비트, 수퍼타이 등"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "비트, 수퍼타이 등",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "오투액션",
       "company": "옥시레킷벤키저",
-      "replacement": "비트, 수퍼타이 등"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "비트, 수퍼타이 등",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "울트라이트",
       "company": "옥시레킷벤키저",
-      "replacement": "비트, 수퍼타이 등"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "비트, 수퍼타이 등",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "쉐리",
       "company": "옥시레킷벤키저",
-      "replacement": "샤프란, 다우니 등"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "샤프란, 다우니 등",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "물먹는 하마",
       "company": "옥시레킷벤키저",
-      "replacement": "물먹는 봉다리"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "물먹는 봉다리",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "데톨",
       "company": "옥시레킷벤키저",
-      "replacement": "아이깨끗해, 해피바스 등"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "아이깨끗해, 해피바스 등",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "게비스콘",
       "company": "옥시레킷벤키저",
-      "replacement": "애시논"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "애시논",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "스트랩실",
       "company": "옥시레킷벤키저",
-      "replacement": "이부프로펜"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "이부프로펜",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     },
     {
       "name": "듀렉스",
       "company": "옥시레킷벤키저",
-      "replacement": "유니더스, 바른생각"
-      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+      "replacement": "유니더스, 바른생각",
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151",
+      "incidentid":1
     }
   ]
 }
