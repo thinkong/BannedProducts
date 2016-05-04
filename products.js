bc8f4b4fc09e76a4673d71b2ff6b5e95f5974ab5
Kyuhun Lee <thinkong@users.noreply.github.com>
2016-05-04 17:49:47 +0900

    update
    data from http://clien.net/cs2/bbs/board.php?bo_table=park&wr_id=46192354&sca=&sfl=wr_subject&stx=%EC%98%A5%EC%8B%9C&page=2

--- products.js
+++ products.js
@@ -1,23 +1,63 @@
 {
   "data": [
     {
-      "name": "OxySakSak",
-      "company": "Oxy",
+      "name": "파워크린",
+      "company": "옥시레킷벤키저",
+      "replacement": "비트, 수퍼타이 등"
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
-      "name": "Oxy2",
-      "company": "Oxy",
+      "name": "옥시크린",
+      "company": "옥시레킷벤키저",
+      "replacement": "비트, 수퍼타이 등"
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
-      "name": "Oxy3",
-      "company": "Oxy",
+      "name": "오투액션",
+      "company": "옥시레킷벤키저",
+      "replacement": "비트, 수퍼타이 등"
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     },
     {
-      "name": "Oxy4",
-      "company": "Oxy",
+      "name": "울트라이트",
+      "company": "옥시레킷벤키저",
+      "replacement": "비트, 수퍼타이 등"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+    },
+    {
+      "name": "쉐리",
+      "company": "옥시레킷벤키저",
+      "replacement": "샤프란, 다우니 등"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+    },
+    {
+      "name": "물먹는 하마",
+      "company": "옥시레킷벤키저",
+      "replacement": "물먹는 봉다리"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+    },
+    {
+      "name": "데톨",
+      "company": "옥시레킷벤키저",
+      "replacement": "아이깨끗해, 해피바스 등"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+    },
+    {
+      "name": "게비스콘",
+      "company": "옥시레킷벤키저",
+      "replacement": "애시논"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+    },
+    {
+      "name": "스트랩실",
+      "company": "옥시레킷벤키저",
+      "replacement": "이부프로펜"
+      "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
+    },
+    {
+      "name": "듀렉스",
+      "company": "옥시레킷벤키저",
+      "replacement": "유니더스, 바른생각"
       "reason": "http://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=102&oid=001&aid=0008376151"
     }
   ]
